629887eaa23b243ab9327665f66c22c53414e628
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2025-04-16 17:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(3000, '0.0.0.0', () => {
+  console.log("App running on 0000/3000");
 });
